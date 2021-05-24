ddf26f0cc7b97ff0adfb6212b8769ef88b088439
손성호 <jujujo79@gmail.com>
2021-05-25 00:21:33 +0900

    초기 커밋.

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(85, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(106, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(206, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(69, 45);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "연결";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(206, 221);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(69, 45);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "종료";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "IP 주소";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM7";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(37, 128);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(154, 136);
+            this.listBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 294);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.IO.Ports;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public const int port = 32000; // 포트
+        Socket socket = null;
+        TcpListener server = null;
+        Thread th = null;
+        TcpClient cl;
+        NetworkStream ns;
+        StreamReader sr;
+        StreamWriter sw;
+        String a = null;
+        Thread th2;
+        Thread th3;
+        public void Run() // 서버 실행
+        {
+            String myaddress = "211.220.184.10"; // ip주소
+            IPAddress localAddr = IPAddress.Parse(myaddress);
+            try
+            {
+                server = new TcpListener(localAddr, port);
+                server.Start();
+                listBox1.Items.Add("연결 대기..");
+            } 
+            catch(Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+
+           
+
+            cl = server.AcceptTcpClient();
+
+            if (cl.Connected)
+            {
+                listBox1.Items.Add("연결 완료!!");
+                ns = cl.GetStream();
+                sr = new StreamReader(ns);
+                sw = new StreamWriter(ns);
+            }
+
+            th2 = new Thread(new ThreadStart(run2)); // 서버스레드 생성
+            th2.Start();
+            th3 = new Thread(new ThreadStart(run3));
+            th3.Start();
+        }
+        public void run2()
+        {
+            while (true)
+            {
+                try
+                {
+                    a = serialPort1.ReadLine();
+
+                    String[] b = a.Split(' ');
+                    listBox1.Items.Add(b[0].Length);
+                    b[1] = b[1].Remove(b[1].Length-1);
+                    listBox1.Items.Add(b[1].Length);
+
+                    sw.WriteLine(b[0]);
+                    sw.WriteLine(b[1]);
+
+                    sw.Flush();
+                }
+                catch (Exception e)
+                {
+                    listBox1.Items.Add("연결 끊기..");
+                    MessageBox.Show(e.ToString());
+                    break;
+                }
+            }
+        }
+
+        public void run3()
+        {
+            while(true)
+            {
+                String b = sr.ReadLine();
+                String[] data = b.Split(' ');
+               
+                if (data[0].Equals("0"))
+                {
+                    serialPort1.Write(b);
+                    listBox1.Items.Add("전송");
+
+                }
+                else if (data[0].Equals("1"))
+                {
+                    serialPort1.Write(b);
+                    listBox1.Items.Add("전송");
+                }
+            }              
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+
+            serialPort1.BaudRate = 9600;
+            serialPort1.DataBits = 8;
+            serialPort1.Parity = Parity.None;
+            serialPort1.StopBits = StopBits.One;
+            serialPort1.Open();
+          
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (serialPort1 != null && serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) // 연결 버튼
+        {
+            th = new Thread(new ThreadStart(Run)); // 서버스레드 생성
+            th.Start();
+            button1.Enabled = false;
+        }
+
+
+        private void button3_Click(object sender, EventArgs e) // 종료 버튼
+        {
+            if (socket != null)
+            {
+                if (socket.Connected)
+                {
+                    socket.Close();
+                    th.Abort();
+                    th2.Abort();
+                    th3.Abort();                  
+                }
+            }
+            Application.Exit();
+        }
+    }
+}
